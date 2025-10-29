f1c3e202767c932a2f87a4b0f9ca3149edcca8fb
Jan-Erik Andersen <mail@andersen-webworks.de>
2025-10-29 23:36:20 +0100

    Auto commit

--- webworks-theme/tierliebe-parts/header.php
+++ webworks-theme/tierliebe-parts/header.php
@@ -26,7 +26,7 @@ bloginfo('name'); ?></title>
 <header class="header">
     <div class="header-content">
         <a href="<?php echo home_url('/'); ?>" class="logo">
-            <span class="logo-icon">🐾</span>Wa(h)re <div>Haustier(liebe)</div>
+            <span class="logo-icon">🐾</span><div>Wa(h)re</div> <div>Haustier(liebe)</div>
         </a>
 
         <nav class="main-nav main-nav-desktop">
